8eb5ae771c2f6bd9e6d0a005872999edd38ba058
Marcela <marcelapereirads@gmail.com>
2021-05-15 01:37:58 -0300

    Moving button component from external project to library

--- projects/basic-component-lib/src/public-api.ts
+++ projects/basic-component-lib/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of basic-component-lib
  */
 
-export * from './lib/basic-component-lib.service';
-export * from './lib/basic-component-lib.component';
-export * from './lib/basic-component-lib.module';
+export * from './lib/button/button.module';
