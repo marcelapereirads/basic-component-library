04603dcc1f285455f879c8e93f7ef303cccbb36e
Marcela Silva <marcelapereirads@gmail.com>
2022-06-02 22:59:25 +0100

    remove mask from input directive

--- projects/basic-component-library/src/lib/base-input/base-input.module.ts
+++ projects/basic-component-library/src/lib/base-input/base-input.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NgxMaskModule } from 'ngx-mask';
-
 import { BaseInputDirective } from './base-input.directive';
 import { BaseErrorModule } from '../base-error/base-error.module';
 
-export const ngxMaskModule = NgxMaskModule.forRoot();
-
 @NgModule({
   declarations: [BaseInputDirective],
-  imports: [CommonModule, ngxMaskModule, FormsModule, ReactiveFormsModule, BaseErrorModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, BaseErrorModule],
   exports: [BaseInputDirective],
 })
 export class BaseInputModule {}
